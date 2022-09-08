25a0911519c7d2a6064d7475fbc5b1f70f60a005
Thomas Honeyman <admin@thomashoneyman.com>
2022-09-07 22:13:56 -0400

    Remove named instances

--- src/Slug.purs
+++ src/Slug.purs
@@ -30,17 +30,17 @@ import Data.String.Pattern (Pattern(..), Replacement(..))
 -- | Example: `Slug "this-is-an-article-slug"`
 newtype Slug = Slug String
 
-derive instance eqSlug :: Eq Slug
-derive instance ordSlug :: Ord Slug
-derive newtype instance semigroupSlug :: Semigroup Slug
+derive Eq Slug
+derive Ord Slug
+derive Semigroup Slug
 
-instance showSlug :: Show Slug where
+instance Show Slug where
   show (Slug str) = "(Slug " <> str <> ")"
 
-instance encodeJsonSlug :: EncodeJson Slug where
+instance EncodeJson Slug where
   encodeJson (Slug s) = encodeJson s
 
-instance decodeJsonSlug :: DecodeJson Slug where
+instance DecodeJson Slug where
   decodeJson = note (TypeMismatch "Slug") <<< parse <=< decodeJson
 
 -- | Create a `Slug` from a string. This will transform the input string
